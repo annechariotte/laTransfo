6769e5f688f66754f12256eaecc6cbf8e0f32edf
Anne-Charlotte <ac.drevensek.deldicque@gmail.com>
2020-07-04 15:29:49 +0200

    feat: add marquee scroll

--- script.js
+++ script.js
@@ -6,27 +6,97 @@ function docReady(fn) {
     } else {
         document.addEventListener("DOMContentLoaded", fn);
     }
-}
+};
+
+function openTab(parent, parentTop) {
+
+    parent.classList.add("show");
+    window.scrollTo(0, parentTop);
+
+};
+
+function closeTab(currentShow) {
+
+    currentShow.classList.remove("show");
+    window.scrollTo(0, 100);
+};
+
+function startScrolling(currentTitle, fixedTitle, marqueeTitle, marqueeContent) {
+
+    currentTitle.appendChild(marqueeTitle);
+    marqueeTitle.appendChild(marqueeContent);
+    currentTitle.removeChild(fixedTitle);
+
+};
+
+function stopScrolling(currentTitle, fixedTitle, marqueeTitle) {
+
+    currentTitle.removeChild(marqueeTitle);
+    currentTitle.appendChild(fixedTitle);
+
+};
 
 docReady(function () {
     // DOM is loaded and ready for manipulation here
-    for (let $i = 0; $i < 7; $i++) {
+    for (let $i = 0; $i <= 6; $i++) {
+
         let header = document.getElementById('section' + $i);
         let parent = header.parentElement;
         let parentTop = parent.offsetTop;
 
-        header.addEventListener('click', function () {
-            let currentShow = document.getElementsByClassName('show')[0];
+        let currentTitle = document.getElementsByClassName("tab-title" + $i)[0];
+        let fixedTitle = currentTitle.getElementsByTagName("h1")[0];
+        let titleContent = fixedTitle.textContent;
+        let marqueeTitle = document.createElement("marquee");
+        let marqueeContent = document.createElement("h1");
+        marqueeContent.textContent = titleContent + " " + titleContent + " " + titleContent + " " + titleContent;
+
+        let show;
+
+        header.addEventListener("click", function () {
+
+            let currentShow = document.getElementsByClassName("show")[0];
 
             if (currentShow) {
-                currentShow.classList.remove('show');
-                window.scrollTo(0, 100);
+
+                closeTab(currentShow)
+                return show = false;
             };
 
             if (currentShow != parent) {
-                parent.classList.add('show');
-                window.scrollTo(0, parentTop);
+
+                openTab(parent, parentTop)
+                return show = true;
             }
-        })
-    }
+        });
+
+        header.addEventListener("mouseenter", function () {
+
+            let defile = false;
+
+            if (!show) {
+                if (!defile) {
+
+                    startScrolling(currentTitle, fixedTitle, marqueeTitle, marqueeContent);
+                    defile = true;
+
+                    if (defile) {
+                        header.addEventListener("click", function () {
+
+                            stopScrolling(currentTitle, fixedTitle, marqueeTitle)
+                            defile = false;
+                        })
+
+                        header.addEventListener("mouseleave", function () {
+
+                            stopScrolling(currentTitle, fixedTitle, marqueeTitle)
+                            defile = false;
+                        });
+                    };
+
+                    return defile;
+                };
+            }
+        });
+    };
 });
