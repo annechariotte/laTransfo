d8723bad62d72d2624861df7333722400a193d08
Anne-Charlotte <ac.drevensek.deldicque@gmail.com>
2020-07-27 17:20:13 +0200

    feat: eventListener Legend "About"

--- js/about-script.js
+++ js/about-script.js
@@ -1,20 +1,35 @@
-const involve = document.getElementById("involve");
-const multi = document.getElementById("multi");
-const rhythm = document.getElementById("rhythm");
-const concrete = document.getElementById("concrete");
+const experimental = document.getElementById("experimental");
+const involveAndForm = document.getElementById("involveAndForm");
+const multidisciplinary = document.getElementById("multidisciplinary");
+const programRhythm = document.getElementById("programRhythm");
+const concreteCase = document.getElementById("concreteCase");
+const ecosysteme = document.getElementById("ecosysteme");
+const project = document.getElementById("project");
 
-involve.addEventListener("click", function () {
+experimental.addEventListener("click", function () {
+    window.open("index.html?tab=experimental");
+});
+
+involveAndForm.addEventListener("click", function () {
     window.open("index.html?tab=involveAndForm");
 });
 
-multi.addEventListener("click", function () {
+multidisciplinary.addEventListener("click", function () {
     window.open("index.html?tab=multidisciplinary");
 });
 
-rhythm.addEventListener("click", function () {
+programRhythm.addEventListener("click", function () {
     window.open("index.html?tab=programRhythm");
 });
 
-concrete.addEventListener("click", function () {
+concreteCase.addEventListener("click", function () {
     window.open("index.html?tab=concreteCase");
+});
+
+ecosysteme.addEventListener("click", function () {
+    window.open("index.html?tab=ecosysteme");
+});
+
+project.addEventListener("click", function () {
+    window.open("index.html?tab=project");
 });
