c07b75a7d64df951cfe03b126800dda3a7e3c77e
Anne-Charlotte <ac.drevensek.deldicque@gmail.com>
2020-07-30 19:34:51 +0200

    feat: texticon "défis"

--- js/about-script.js
+++ js/about-script.js
@@ -6,30 +6,59 @@ const concreteCase = document.getElementById("concreteCase");
 const ecosysteme = document.getElementById("ecosysteme");
 const project = document.getElementById("project");
 
+const experimentalMobile = document.getElementById("experimental-mobile");
+const involveAndFormMobile = document.getElementById("involveAndForm-mobile");
+const multidisciplinaryMobile = document.getElementById("multidisciplinary-mobile");
+const programRhythmMobile = document.getElementById("programRhythm-mobile");
+const concreteCaseMobile = document.getElementById("concreteCase-mobile");
+const ecosystemeMobile = document.getElementById("ecosysteme-mobile");
+const projectMobile = document.getElementById("project-mobile");
+
 experimental.addEventListener("click", function () {
+    window.open("index.html?tab=experimental");
+});
+experimentalMobile.addEventListener("click", function () {
     window.open("index.html?tab=experimental");
 });
 
 involveAndForm.addEventListener("click", function () {
     window.open("index.html?tab=involveAndForm");
 });
+involveAndFormMobile.addEventListener("click", function () {
+    window.open("index.html?tab=involveAndForm");
+});
 
 multidisciplinary.addEventListener("click", function () {
+    window.open("index.html?tab=multidisciplinary");
+});
+multidisciplinaryMobile.addEventListener("click", function () {
     window.open("index.html?tab=multidisciplinary");
 });
 
 programRhythm.addEventListener("click", function () {
     window.open("index.html?tab=programRhythm");
 });
+programRhythmMobile.addEventListener("click", function () {
+    window.open("index.html?tab=programRhythm");
+});
 
 concreteCase.addEventListener("click", function () {
+    window.open("index.html?tab=concreteCase");
+});
+concreteCaseMobile.addEventListener("click", function () {
     window.open("index.html?tab=concreteCase");
 });
 
 ecosysteme.addEventListener("click", function () {
     window.open("index.html?tab=ecosysteme");
 });
+ecosystemeMobile.addEventListener("click", function () {
+    window.open("index.html?tab=ecosysteme");
+});
 
 project.addEventListener("click", function () {
+    window.open("index.html?tab=project");
+});
+projectMobile.addEventListener("click", function () {
     window.open("index.html?tab=project");
 });
