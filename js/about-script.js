e891884655de462a73e9e72eb8d1bb0fe6dbc3e8
Anne-Charlotte <ac.drevensek.deldicque@gmail.com>
2020-09-08 12:11:47 +0200

    feat: about-script function

--- js/about-script.js
+++ js/about-script.js
@@ -1,78 +1,89 @@
 const experimental = document.getElementById("experimental");
-const involveAndForm = document.getElementById("involveAndForm");
-const multidisciplinary = document.getElementById("multidisciplinary");
-const programRhythm = document.getElementById("programRhythm");
-const concreteCase = document.getElementById("concreteCase");
-const ecosysteme = document.getElementById("ecosysteme");
-const project = document.getElementById("project");
-
 const experimentalMobile = document.getElementById("experimental-mobile");
-const involveAndFormMobile = document.getElementById("involveAndForm-mobile");
-const multidisciplinaryMobile = document.getElementById("multidisciplinary-mobile");
-const programRhythmMobile = document.getElementById("programRhythm-mobile");
-const concreteCaseMobile = document.getElementById("concreteCase-mobile");
-const ecosystemeMobile = document.getElementById("ecosysteme-mobile");
-const projectMobile = document.getElementById("project-mobile");
-
 if (experimental) {
     experimental.addEventListener("click", function () {
         window.open("index.html?tab=experimental");
     })
-
+}
+if (experimentalMobile) {
     experimentalMobile.addEventListener("click", function () {
         window.open("index.html?tab=experimental");
     })
 };
 
-involveAndForm.addEventListener("click", function () {
-    window.open("index.html?tab=involveAndForm");
-});
+const involveAndForm = document.getElementById("involveAndForm");
+const involveAndFormMobile = document.getElementById("involveAndForm-mobile");
+if (involveAndForm) {
+    involveAndForm.addEventListener("click", function () {
+        window.open("index.html?tab=involveAndForm");
+    });
+}
 if (involveAndFormMobile) {
     involveAndFormMobile.addEventListener("click", function () {
         window.open("index.html?tab=involveAndForm");
     })
 };
 
-multidisciplinary.addEventListener("click", function () {
-    window.open("index.html?tab=multidisciplinary");
-});
+const multidisciplinary = document.getElementById("multidisciplinary");
+const multidisciplinaryMobile = document.getElementById("multidisciplinary-mobile");
+if (multidisciplinary) {
+    multidisciplinary.addEventListener("click", function () {
+        window.open("index.html?tab=multidisciplinary");
+    });
+}
 if (multidisciplinaryMobile) {
     multidisciplinaryMobile.addEventListener("click", function () {
         window.open("index.html?tab=multidisciplinary");
     })
 };
 
-programRhythm.addEventListener("click", function () {
-    window.open("index.html?tab=programRhythm");
-});
+const programRhythm = document.getElementById("programRhythm");
+const programRhythmMobile = document.getElementById("programRhythm-mobile");
+if (programRhythm) {
+    programRhythm.addEventListener("click", function () {
+        window.open("index.html?tab=programRhythm");
+    });
+}
 if (programRhythmMobile) {
     programRhythmMobile.addEventListener("click", function () {
         window.open("index.html?tab=programRhythm");
     })
 };
 
-concreteCase.addEventListener("click", function () {
-    window.open("index.html?tab=concreteCase");
-});
+const concreteCase = document.getElementById("concreteCase");
+const concreteCaseMobile = document.getElementById("concreteCase-mobile");
+if (concreteCase) {
+    concreteCase.addEventListener("click", function () {
+        window.open("index.html?tab=concreteCase");
+    });
+}
 if (concreteCaseMobile) {
     concreteCaseMobile.addEventListener("click", function () {
         window.open("index.html?tab=concreteCase");
     })
 };
 
+const ecosysteme = document.getElementById("ecosysteme");
+const ecosystemeMobile = document.getElementById("ecosysteme-mobile");
 if (ecosysteme) {
     ecosysteme.addEventListener("click", function () {
         window.open("index.html?tab=ecosysteme");
     });
+}
+if (ecosystemeMobile) {
     ecosystemeMobile.addEventListener("click", function () {
         window.open("index.html?tab=ecosysteme");
     })
 };
 
+const project = document.getElementById("project");
+const projectMobile = document.getElementById("project-mobile");
 if (project) {
     project.addEventListener("click", function () {
         window.open("index.html?tab=project");
     });
+}
+if (programRhythmMobile) {
     projectMobile.addEventListener("click", function () {
         window.open("index.html?tab=project");
     })
