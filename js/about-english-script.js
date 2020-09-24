7e49b5b395fe5525628e323ff7147fbf51aac750
Anne-Charlotte <ac.drevensek.deldicque@gmail.com>
2020-09-24 20:29:32 +0200

    feat: english open tab script

---
+++ js/about-english-script.js
@@ -0,0 +1,90 @@
+const experimental = document.getElementById("experimental");
+const experimentalMobile = document.getElementById("experimental-mobile");
+if (experimental) {
+    experimental.addEventListener("click", function () {
+        window.open("english.html?tab=experimental");
+    })
+}
+if (experimentalMobile) {
+    experimentalMobile.addEventListener("click", function () {
+        window.open("english.html?tab=experimental");
+    })
+};
+
+const involveAndForm = document.getElementById("involveAndForm");
+const involveAndFormMobile = document.getElementById("involveAndForm-mobile");
+if (involveAndForm) {
+    involveAndForm.addEventListener("click", function () {
+        window.open("english.html?tab=involveAndForm");
+    });
+}
+if (involveAndFormMobile) {
+    involveAndFormMobile.addEventListener("click", function () {
+        window.open("english.html?tab=involveAndForm");
+    })
+};
+
+const multidisciplinary = document.getElementById("multidisciplinary");
+const multidisciplinaryMobile = document.getElementById("multidisciplinary-mobile");
+if (multidisciplinary) {
+    multidisciplinary.addEventListener("click", function () {
+        window.open("english.html?tab=multidisciplinary");
+    });
+}
+if (multidisciplinaryMobile) {
+    multidisciplinaryMobile.addEventListener("click", function () {
+        window.open("english.html?tab=multidisciplinary");
+    })
+};
+
+const programPace = document.getElementById("programPace");
+const programPaceMobile = document.getElementById("programPace-mobile");
+if (programPace) {
+    programPace.addEventListener("click", function () {
+        window.open("english.html?tab=programPace");
+    });
+}
+if (programPaceMobile) {
+    programPaceMobile.addEventListener("click", function () {
+        window.open("english.html?tab=programPace");
+    })
+};
+
+const concreteCase = document.getElementById("concreteCase");
+const concreteCaseMobile = document.getElementById("concreteCase-mobile");
+if (concreteCase) {
+    concreteCase.addEventListener("click", function () {
+        window.open("english.html?tab=concreteCase");
+    });
+}
+if (concreteCaseMobile) {
+    concreteCaseMobile.addEventListener("click", function () {
+        window.open("english.html?tab=concreteCase");
+    })
+};
+
+const ecosysteme = document.getElementById("ecosysteme");
+const ecosystemeMobile = document.getElementById("ecosysteme-mobile");
+if (ecosysteme) {
+    ecosysteme.addEventListener("click", function () {
+        window.open("english.html?tab=ecosysteme");
+    });
+}
+if (ecosystemeMobile) {
+    ecosystemeMobile.addEventListener("click", function () {
+        window.open("english.html?tab=ecosysteme");
+    })
+};
+
+const project = document.getElementById("project");
+const projectMobile = document.getElementById("project-mobile");
+if (project) {
+    project.addEventListener("click", function () {
+        window.open("english.html?tab=project");
+    });
+}
+if (programPaceMobile) {
+    projectMobile.addEventListener("click", function () {
+        window.open("english.html?tab=project");
+    })
+};
